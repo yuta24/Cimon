4ece88e08d002f3c3a9d8d7e06c4ef7ea455718e
yuta24 <yuta24@bivre.com>
2019-05-04 01:11:41 +0900

    Initialize project

---
+++ Cimon/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Cimon
+//
+//  Created by Yu Tawata on 2019/05/04.
+//  Copyright © 2019 Yu Tawata. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
