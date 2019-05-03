b241440a757a16847ef926971b1e764fcb5d8c6a
yuta24 <yuta24@bivre.com>
2019-05-04 01:32:28 +0900

    Add frameworks

---
+++ App/App.h
@@ -0,0 +1,19 @@
+//
+//  App.h
+//  App
+//
+//  Created by Yu Tawata on 2019/05/04.
+//  Copyright © 2019 Yu Tawata. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for App.
+FOUNDATION_EXPORT double AppVersionNumber;
+
+//! Project version string for App.
+FOUNDATION_EXPORT const unsigned char AppVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <App/PublicHeader.h>
+
+
---
+++ Domain/Domain.h
@@ -0,0 +1,19 @@
+//
+//  Domain.h
+//  Domain
+//
+//  Created by Yu Tawata on 2019/05/04.
+//  Copyright © 2019 Yu Tawata. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Domain.
+FOUNDATION_EXPORT double DomainVersionNumber;
+
+//! Project version string for Domain.
+FOUNDATION_EXPORT const unsigned char DomainVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Domain/PublicHeader.h>
+
+
