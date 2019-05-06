533e9aa1946abece6cf996e9788bb3bd1f686592
yuta24 <yuta24@bivre.com>
2019-05-06 14:44:45 +0900

    Impl settings

--- App/Sources/Scenes/Settings/SettingsViewController.swift
+++ App/Sources/Scenes/Settings/SettingsViewController.swift
@@ -13,6 +13,27 @@ class SettingsViewController: UIViewController, Instantiatable {
     struct Dependency {
     }
 
+    @IBOutlet weak var tableView: UITableView! {
+        didSet {
+            tableView.tableFooterView = UIView()
+            tableView.dataSource = self
+            tableView.delegate = self
+        }
+    }
+
     func inject(dependency: SettingsViewController.Dependency) {
     }
+}
+
+extension SettingsViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+}
+
+extension SettingsViewController: UITableViewDelegate {
 }
