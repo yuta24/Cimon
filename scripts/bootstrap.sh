5f0546f313d853aaf2dd6008bf73555af8e92cc7
yuta24 <yuta24@bivre.com>
2019-06-24 00:53:35 +0900

    Change project.yml to project.swift

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -7,10 +7,8 @@ PROJECT_NAME=Cimon
 # bootstrap
 
 echo "  + Generate xcodeproje by XcodeGen."
-mint run --silent yonaskolb/XcodeGen xcodegen
-if [ "${CI-A}" = "A" ]; then
-  mint run --silent Carthage/Carthage carthage bootstrap --platform iOS --cache-builds
-fi
+mint run --silent mxcl/swift-sh swift-sh `dirname $0`/../project.swift
+mint run --silent Carthage/Carthage carthage bootstrap --platform iOS --cache-builds
 bundle exec pod install
 
 echo ""
