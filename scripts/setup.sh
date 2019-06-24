fd81bb8537bb206dbb7f49cc87d6492fad1ee8cf
yuta24 <yuta24@bivre.com>
2019-06-24 17:18:33 +0900

    Improve hook

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -2,7 +2,8 @@
 
 set -eu
 
-cp -R scripts/_hooks/* .git/hooks/
+# cp -R scripts/_hooks/* .git/hooks/
+ln -sf scripts/_hooks/pre-push .git/hooks/pre-push
 
 brew install libxml2 # For SwiftGen via Mint
 
