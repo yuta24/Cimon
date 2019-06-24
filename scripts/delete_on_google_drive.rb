39c436da72ed79f37b2b35543540da569f1681a9
yuta24 <yuta24@bivre.com>
2019-06-24 22:42:19 +0900

    Improve Cache script

---
+++ scripts/delete_on_google_drive.rb
@@ -0,0 +1,7 @@
+require_relative '_cache/google_drive'
+
+hash = %x(cat Cartfile.resolved Podfile.lock | openssl sha256).strip
+file_name = "Cimon-#{hash}.tar.gz"
+
+google_drive = GoogleDrive.new('1OU6TDa4CCrX3cSYqJjxCdueA3DEHXI9R')
+google_drive.delete(file_name)
---
+++ scripts/upload_to_google_drive.rb
@@ -0,0 +1,10 @@
+require_relative '_cache/google_drive'
+
+hash = %x(cat Cartfile.resolved Podfile.lock | openssl sha256).strip
+file_name = "Cimon-#{hash}.tar.gz"
+system("tar czf #{file_name} Carthage/Build Pods")
+
+google_drive = GoogleDrive.new('1OU6TDa4CCrX3cSYqJjxCdueA3DEHXI9R')
+google_drive.upload(file_name)
+
+system("rm #{file_name}")
