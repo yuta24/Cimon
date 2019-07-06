60f2e0ddd6a2cb4647e5e94d8c846ef0a1162dce
yuta24 <yuta24@bivre.com>
2019-07-06 14:24:32 +0900

    Install chisel

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -7,11 +7,20 @@ ln -sf ../../scripts/hooks/pre-push .git/hooks/pre-push
 brew install libxml2 # For SwiftGen via Mint
 
 # Mint
-if [ ! $(which mint) ];then
+if [ ! $(which mint) ]; then
   echo "  + Installing mint..."
   brew install mint
 else
   echo "  + Mint found."
+fi
+
+# chisel
+if [ ! $(which chisel) ]; then
+  echo "  + Installing chisel..."
+  brew install chisel
+  defaults write com.apple.dt.lldb DefaultPythonVersion 2
+else
+  echo "  + chisel found."
 fi
 
 bundle install --path vendor/bundle
