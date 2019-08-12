71b0d77e0b37663b42e091855578d14fca1e9de6
yuta24 <yuta24@bivre.com>
2019-08-13 08:23:31 +0900

    WIP

--- App/Sources/Scenes/BuildLog/BuildLogViewController.swift
+++ App/Sources/Scenes/BuildLog/BuildLogViewController.swift
@@ -6,3 +6,22 @@
 //
 
 import Foundation
+import UIKit
+import Pipeline
+import Shared
+import Domain
+
+class BuildLogViewController: UIViewController {
+    @IBOutlet weak var contentView: UIView!
+    @IBOutlet weak var textView: UITextView! {
+        didSet {
+            apply(textView, terminalStyle)
+        }
+    }
+
+    @IBOutlet weak var activityIndicatorView: UIActivityIndicatorView! {
+        didSet {
+            activityIndicatorView.hidesWhenStopped = true
+        }
+    }
+}
