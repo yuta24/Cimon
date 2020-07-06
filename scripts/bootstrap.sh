ae2350b30bc859b5924e5551dde76b89cde140ff
yuta24 <yuta24@bivre.com>
2020-07-07 07:45:05 +0900

    Fix

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -10,7 +10,7 @@ PROJECT_NAME=Cimon
 
 echo "  + Generate xcodeproje by XcodeGen."
 xcodegen
-carthage bootstrap --platform iOS --no-use-binaries --cache-builds
+carthage bootstrap --platform iOS --no-use-binaries --cache-builds --use-ssh
 bundle exec pod install
 
 echo ""
