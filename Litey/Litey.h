1d9af95d09de501d72e3ce33fe49f70d9e011206
yuta24 <yuta24@bivre.com>
2020-08-10 21:31:09 +0900

    Add Litey framework

---
+++ Litey/Litey.h
@@ -0,0 +1,18 @@
+//
+//  Litey.h
+//  Litey
+//
+//  Created by Yu Tawata on 2020/08/10.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Litey.
+FOUNDATION_EXPORT double LiteyVersionNumber;
+
+//! Project version string for Litey.
+FOUNDATION_EXPORT const unsigned char LiteyVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Litey/PublicHeader.h>
+
+
